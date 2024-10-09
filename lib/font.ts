a9b1beb118217f93269058d10956dc742bb0fa90
Sly Villafania <villafania.sly.lau@gmail.com>
2024-10-09 17:08:57 +0800

    Create font file for easy access

---
+++ lib/font.ts
@@ -0,0 +1,5 @@
+import { DM_Sans, Jersey_25 } from "next/font/google";
+
+export const DMSans = DM_Sans({ subsets: ["latin"] });
+
+export const Jersey25 = Jersey_25({ weight: "400", subsets: ["latin"] });
