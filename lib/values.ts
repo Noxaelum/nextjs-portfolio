f7528f60b322fbb523df3aaf43407dcb81f03317
Sly Villafania <villafania.sly.lau@gmail.com>
2024-10-09 17:07:56 +0800

    Create base values file

---
+++ lib/values.ts
@@ -0,0 +1,15 @@
+export const NAV_LINK_VALUES = [
+  { link: "#home", text: "Home" },
+  { link: "#experience", text: "Experiences" },
+  { link: "#skills", text: "Skills" },
+  { link: "#about-me", text: "About me" },
+  { link: "#contacts", text: "Contacts" },
+];
+
+export const HOME_VALUES = [
+  { section: "home", content: "Home" },
+  { section: "experience", content: "Experiences" },
+  { section: "skills", content: "Skills" },
+  { section: "about-me", content: "About me" },
+  { section: "contacts", content: "Contacts" },
+];
